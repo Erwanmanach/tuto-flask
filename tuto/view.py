e09e4e75c4a9b71cae3dab2946de30383f81506b
Erwanmanach <erwan.manach@etu.univ-orleans.fr>
2022-11-15 12:21:39 +0100

    fin de la feuille 1

--- tuto/view.py
+++ tuto/view.py
@@ -1,4 +1,27 @@
 from .app import app
+import yaml , os.path
+from yaml import Loader
+from flask import render_template
+
 @app.route("/")
 def home():
-    return "<h1 > Hello World </h1 >"
+    return render_template(
+            "home.html",
+            title ="Hello World!",
+            names =["Pierre", "Paul", "Corinne"]
+    )
+
+# @app.route("/")
+# def template2():
+#     print(os.path.join(os.path.dirname(__file__),"data.yml"))
+#     return render_template(
+#             "templates2.html",
+#             title = "Les 100 meilleurs bouqin de SF",
+#             data = yaml.load(open(
+#                         os.path.join(
+#                         os.path.dirname(__file__),
+#                         "data.yml"
+#                         )
+#                     ), Loader=Loader
+#                 )
+#             )
